5a7fb1d0f1b05f68e9923f6e210d515b317161a5
Timm Schoenborn <timmschoenborn@gmail.com>
2023-10-04 13:43:51 -0400

    removes auth requirement on show pet route

--- app/routes/pet_routes.js
+++ app/routes/pet_routes.js
@@ -54,7 +54,7 @@ router.get('/pets/mine', requireToken, (req, res, next) => {
 
 // SHOW
 // GET /pets/5a7db6c74d55bc51bdf39793
-router.get('/pets/:id', requireToken, (req, res, next) => {
+router.get('/pets/:id', (req, res, next) => {
 	// req.params.id will be set based on the `:id` in the route
 	Pet.findById(req.params.id)
 		.then(handle404)
